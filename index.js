830f9d773d3304f53e67dd34dcacb17ba4cebf85
Fa Wren Chong <fwc14@uclive.ac.nz>
2023-12-08 21:40:49 +1300

    Added different play modes

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const {
     prefix, 
     token,
+    tracksPath
 } = require('./config.json');
+
+const fs = require.apply('fs');
 
 const client = new Discord.Client({
     intents: [
@@ -24,15 +27,26 @@ client.once('disconnect', () => {
     console.log('Disconnected');
 });
 
+const queue = new Map();
+
 client.on('messageCreate', message => {
-    console.log('Message');
+    const serverQueue = queue.get(message.guildId);
+
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     if (message.content.startsWith(`${prefix}ping`)) {
         console.log('Ping');
     }
-    else if (message.content.startsWith(`${prefix}play`)) {
+    else if (message.content.startsWith(`${prefix}play-normal`)) {
+        playTracks(message, tracksPath.normal, serverQueue);
         return;
+    }
+    else if (message.content.startsWith(`${prefix}play-combat`)) {
+        playTracks(message, tracksPath.combat, serverQueue);
+        return;
+    }
+    else if (message.content.startsWith(`${prefix}play-jolly`)) {
+        playTracks(message, tracksPath.jolly, serverQueue);
     }
     else if (message.content.startsWith(`${prefix}pause`)) {
         return;
@@ -47,5 +61,32 @@ client.on('messageCreate', message => {
         message.channel.send('Enter a valid command');
     }
 });
+
+const queueContract = {
+    textChannel: message.channel,
+    voiceChannel: voiceChannel,
+    connection: null,
+    songs: [],
+    volume: 5,
+    playing: true,
+};
+
+async function playTracks (message, path, serverQueue) {
+    const voiceChannel = message.member.voice.channel;
+    const permissions = voiceChannel.permissionsFor(message.client.user)
+    const tracks = fs.readdirSync(path);
+
+    if (!voiceChannel) {
+        return message.channel.send("Please join a voice channel");
+    }
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send("The bot needs permissions to connect and speak in voice channel");
+    }
+
+    if (!serverQueue) {
+        
+    }
+    return;
+}
 
 client.login(token);
