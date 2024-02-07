93f71da488b3a115199f6d60c7ff1fbef90e8e9c
Fa Wren Chong <fwc14@uclive.ac.nz>
2024-02-07 19:54:14 +1300

    Fixed SlashCommandBuilder import, and added command interaction handling

--- index.js
+++ index.js
@@ -57,10 +57,30 @@ client.once('disconnect', () => {
 });
 
 // respond to interactions. Not all interactions are slash commands, so this functiion only handles slash commands. 
-client.on(Discord.Events.InteractionCreate, interaction => {
+client.on(Discord.Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } 
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command.', ephemeral: true});
+        }
+        else {
+            await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
+        }
+    }
+
     console.log(interaction);
-})
+});
 
 const queue = new Map();
 
