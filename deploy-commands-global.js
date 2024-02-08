1a8444beb5e40089991eb2b7e9fcda4229893ffb
Fa Wren Chong <fwc14@uclive.ac.nz>
2024-02-08 22:00:18 +1300

    Added script to deploy commands globally

---
+++ deploy-commands-global.js
@@ -0,0 +1,25 @@
+const { REST, Routes} = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const { get_commands } = require('./get-commands.js');
+
+// This may look like a duplicate of the code in index.js but it uses an array instead of a discord dictionary
+const commands = get_commands();
+
+const rest = new REST().setToken(token);
+
+// deploy the commands
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(clientId, guildId), 
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } 
+    catch (error) {
+        console.error(error);
+    }
+})();
